7ba1961f40cbcba3e1bb373a700b88ff3190bd86
Indira Carman <indika2411@yahoo.com>
2022-12-06 20:23:39 -0500

    removed fake data and replace  it with  database blueprint

--- src/test/java/com/cydeo/TicketingProjectOrmApplicationTests.java
+++ src/test/java/com/cydeo/TicketingProjectOrmApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TicketingProjectOrmApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
